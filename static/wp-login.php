c1ee98842157ca5b8d21203f4b7ada3bc82e0dc3
James Swineson <github@public.swineson.me>
2022-05-04 13:52:21 +0800

    hardcode wp-login.php

--- static/wp-login.php
+++ static/wp-login.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
-<html lang="{{ .Site.Language.Lang }}" dir="{{ .Site.Language.LanguageDirection | default "ltr" }}">
+<html lang="en-US">
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Log In &lsaquo; {{ .Site.Title | safeHTML }} &#8212; WordPress</title>
+    <title>Log In &lsaquo; <?php echo get_bloginfo( 'name' ); ?> &#8212; WordPress</title>
     <meta name='robots' content='max-image-preview:large, noindex, noarchive' />
     <link rel='dns-prefetch' href='//s.w.org' />
     <link rel='stylesheet' id='dashicons-css'
-        href='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/css/dashicons.min.css?ver=1a556f4a62f50039d96ae58c90bbb126'
+        href='/twentytwelve/css/dashicons.min.css?ver=1a556f4a62f50039d96ae58c90bbb126'
         type='text/css' media='all' />
     <link rel='stylesheet' id='buttons-css'
-        href='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/css/buttons.min.css?ver=1a556f4a62f50039d96ae58c90bbb126'
+        href='/twentytwelve/css/buttons.min.css?ver=1a556f4a62f50039d96ae58c90bbb126'
         type='text/css' media='all' />
     <link rel='stylesheet' id='forms-css'
-        href='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/css/forms.min.css?ver=1a556f4a62f50039d96ae58c90bbb126'
+        href='/twentytwelve/css/forms.min.css?ver=1a556f4a62f50039d96ae58c90bbb126'
         type='text/css' media='all' />
     <link rel='stylesheet' id='l10n-css'
-        href='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/css/l10n.min.css?ver=1a556f4a62f50039d96ae58c90bbb126'
+        href='/twentytwelve/css/l10n.min.css?ver=1a556f4a62f50039d96ae58c90bbb126'
         type='text/css' media='all' />
     <link rel='stylesheet' id='login-css'
-        href='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/css/login.min.css?ver=1a556f4a62f50039d96ae58c90bbb126'
+        href='/twentytwelve/css/login.min.css?ver=1a556f4a62f50039d96ae58c90bbb126'
         type='text/css' media='all' />
     <meta name='referrer' content='strict-origin-when-cross-origin' />
     <meta name="viewport" content="width=device-width" />
 </head>
 
 <body class="login no-js login-action-login wp-core-ui locale-en-us">
+Warning: Cannot modify header information – headers already sent by (output started at /var/www/html/wp-content/themes/wp-dental/functions.php:1) in /var/www/html/wp-login.php on line 530
+
+Warning: Cannot modify header information – headers already sent by (output started at /var/www/html/wp-content/themes/wp-dental/functions.php:1) in /var/www/html/wp-includes/pluggable.php on line 1299
     <script type="text/javascript">
         document.body.className = document.body.className.replace('no-js', 'js');
     </script>
@@ -34,7 +37,7 @@
         <div id="login_error"> Please log in again.<br />
         </div>
 
-        <form name="loginform" id="loginform" action="{{ .Site.BaseURL | safeHTMLAttr }}wp-login.php" method="post">
+        <form name="loginform" id="loginform" action="/wp-login.php" method="post">
             <p>
                 <label for="user_login">Username or Email Address</label>
                 <input type="text" name="log" id="user_login" aria-describedby="login_error" class="input" value=""
@@ -57,13 +60,13 @@
             <p class="submit">
                 <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large"
                     value="Log In" />
-                <input type="hidden" name="redirect_to" value="{{ .Site.BaseURL | safeHTMLAttr }}wp-admin/" />
+                <input type="hidden" name="redirect_to" value="/wp-admin/" />
                 <input type="hidden" name="testcookie" value="1" />
             </p>
         </form>
 
         <p id="nav">
-            <a href="{{ .Site.BaseURL | safeHTMLAttr }}wp-login.php?action=lostpassword">Lost your password?</a>
+            <a href="/wp-login.php?action=lostpassword">Lost your password?</a>
         </p>
         <script type="text/javascript">
             function wp_attempt_focus() { setTimeout(function () { try { d = document.getElementById("user_login"); d.focus(); d.select(); } catch (er) { } }, 200); }
@@ -71,7 +74,7 @@
             if (typeof wpOnload === 'function') { wpOnload() }
         </script>
         <p id="backtoblog">
-            <a href="{{ .Site.BaseURL | safeHTMLAttr }}">&larr; Go to {{ .Site.Title | safeHTML }}</a>
+            <a href="/">&larr; Go to <?php echo get_bloginfo( 'name' ); ?></a>
         </p>
     </div>
     <div class="language-switcher">
@@ -91,29 +94,29 @@
 
         </form>
     </div>
-    <script type='text/javascript' src='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/js/jquery/jquery.min.js?ver=3.6.0'
+    <script type='text/javascript' src='/twentytwelve/js/jquery/jquery.min.js?ver=3.6.0'
         id='jquery-core-js'></script>
     <script type='text/javascript'
-        src='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/js/jquery/jquery-migrate.min.js?ver=3.3.2'
+        src='/twentytwelve/js/jquery/jquery-migrate.min.js?ver=3.3.2'
         id='jquery-migrate-js'></script>
     <script type='text/javascript' id='zxcvbn-async-js-extra'>
         /* <![CDATA[ */
-        var _zxcvbnSettings = { "src": "{{ .Site.BaseURL | safeHTMLAttr }}\/twentytwelve\/js\/zxcvbn.min.js" };
+        var _zxcvbnSettings = { "src": "\/twentytwelve\/js\/zxcvbn.min.js" };
         /* ]]> */
     </script>
-    <script type='text/javascript' src='{{ .Site.BaseURL | safeHTMLAttr }}wp-includes/js/zxcvbn-async.min.js?ver=1.0'
+    <script type='text/javascript' src='/twentytwelve/js/zxcvbn-async.min.js?ver=1.0'
         id='zxcvbn-async-js'></script>
     <script type='text/javascript'
-        src='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9'
+        src='/twentytwelve/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.9'
         id='regenerator-runtime-js'></script>
     <script type='text/javascript'
-        src='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0'
+        src='/twentytwelve/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0'
         id='wp-polyfill-js'></script>
     <script type='text/javascript'
-        src='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/js/dist/hooks.min.js?ver=1e58c8c5a32b2e97491080c5b10dc71c'
+        src='/twentytwelve/js/dist/hooks.min.js?ver=1e58c8c5a32b2e97491080c5b10dc71c'
         id='wp-hooks-js'></script>
     <script type='text/javascript'
-        src='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/js/dist/i18n.min.js?ver=30fcecb428a0e8383d3776bcdd3a7834'
+        src='/twentytwelve/js/dist/i18n.min.js?ver=30fcecb428a0e8383d3776bcdd3a7834'
         id='wp-i18n-js'></script>
     <script type='text/javascript' id='wp-i18n-js-after'>
         wp.i18n.setLocaleData({ 'text direction\u0004ltr': ['ltr'] });
@@ -131,9 +134,9 @@
         })("default", { "locale_data": { "messages": { "": {} } } });
     </script>
     <script type='text/javascript'
-        src='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/js/password-strength-meter.min.js?ver=1a556f4a62f50039d96ae58c90bbb126'
+        src='/twentytwelve/js/password-strength-meter.min.js?ver=1a556f4a62f50039d96ae58c90bbb126'
         id='password-strength-meter-js'></script>
-    <script type='text/javascript' src='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/js/underscore.min.js?ver=1.13.1'
+    <script type='text/javascript' src='/twentytwelve/js/underscore.min.js?ver=1.13.1'
         id='underscore-js'></script>
     <script type='text/javascript' id='wp-util-js-extra'>
         /* <![CDATA[ */
@@ -141,7 +144,7 @@
         /* ]]> */
     </script>
     <script type='text/javascript'
-        src='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/js/wp-util.min.js?ver=1a556f4a62f50039d96ae58c90bbb126'
+        src='/twentytwelve/js/wp-util.min.js?ver=1a556f4a62f50039d96ae58c90bbb126'
         id='wp-util-js'></script>
     <script type='text/javascript' id='user-profile-js-extra'>
         /* <![CDATA[ */
@@ -156,7 +159,7 @@
         })("default", { "locale_data": { "messages": { "": {} } } });
     </script>
     <script type='text/javascript'
-        src='{{ .Site.BaseURL | safeHTMLAttr }}twentytwelve/js/user-profile.min.js?ver=1a556f4a62f50039d96ae58c90bbb126'
+        src='/twentytwelve/js/user-profile.min.js?ver=1a556f4a62f50039d96ae58c90bbb126'
         id='user-profile-js'></script>
     <div class="clear"></div>
 </body>
